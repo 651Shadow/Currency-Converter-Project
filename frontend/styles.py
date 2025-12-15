c4a753037301ec69a3375304d4ef0b1930f454cc
mohamedelhindy <mohamed.hiendy@gmail.com>
2025-12-16 00:03:21 +0200

    gui

--- frontend/styles.py
+++ frontend/styles.py
@@ -1,1 +1,5 @@
-# Placeholder module for the Conversion Calculator project.
+from tkinter import ttk
+
+def styling(root):
+    root.tk.call("source", "frontend/theme/theme.tcl")
+    ttk.Style().theme_use("forest-dark")
