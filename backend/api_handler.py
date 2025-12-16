f90c4ecf5a976669cf4d40b16b7dc582797bff11
651Shadow <651Shadow@users.noreply.github.com>
2025-12-16 21:21:25 +0200

    Updated formating and minor updates

--- backend/api_handler.py
+++ backend/api_handler.py
@@ -2,17 +2,14 @@ import requests as req
 import json
 import os
 
-url = r'https://v6.exchangerate-api.com/v6/2baa9d40347d0d8b176f2069/latest/USD'
+url = r"https://v6.exchangerate-api.com/v6/2baa9d40347d0d8b176f2069/latest/USD"
+
 
 def get_currency_data():
 
     try:
         response = req.get(url)
-        json.dump(
-            dict(response.json()),
-            open("currency.json", "w+"),
-            indent=4
-            )
+        json.dump(dict(response.json()), open("currency.json", "w+"), indent=4)
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -21,7 +18,9 @@ def get_currency_data():
         if os.path.exists("currency.json"):
             print("There is a cached currency file available. Using cached data.")
         else:
-            print("No cached currency file found. Please check your internet connection.")
+            print(
+                "No cached currency file found. Please check your internet connection."
+            )
             exit(5)
             return False
 
@@ -30,9 +29,7 @@ def get_currency_data():
         with open("currency.json", "r") as f:
             data = json.load(f)
 
-        cleaned_data = {
-            "conversion_rates": data.get("conversion_rates", {})
-        }
+        cleaned_data = {"conversion_rates": data.get("conversion_rates", {})}
 
         with open("currency.json", "w") as f:
             json.dump(cleaned_data, f, indent=4)
@@ -41,5 +38,7 @@ def get_currency_data():
         print("The currency data may be incomplete or corrupted.")
         return False
     return True
+
+
 if __name__ == "__main__":
     get_currency_data()
--- backend/converters/currency.py
+++ backend/converters/currency.py
@@ -1,1 +1,8 @@
 # Placeholder module for the Conversion Calculator project.
+from ..api_handler import get_currency_data
+
+if not get_currency_data():
+# Check if currency data was fetched and saved successfully
+    print("Failed to update currency data. Exiting.")
+    exit(1)
+
