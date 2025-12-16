8664d678b61d30a1d604805d83e54b8af60901a4
651Shadow <651Shadow@users.noreply.github.com>
2025-12-16 21:52:59 +0200

    Currency converter done, area.py formatting fixes

--- backend/converters/area.py
+++ backend/converters/area.py
@@ -1,15 +1,16 @@
-import math 
+import math
 
 CONVERSTIONS_TO_M_SQUAR = {
-    'm2' : 1,
-    'cm2' :10000 ,
-    'mm2': 1000000,
-    'km2' : 0.000001,
-    'ft2' : 10.7639,
-    'in2' : 1550.0031,
-    'yd2' : 1.19599,
-    'mile2' : 0.000000386102
+    "m2": 1,
+    "cm2": 10000,
+    "mm2": 1000000,
+    "km2": 0.000001,
+    "ft2": 10.7639,
+    "in2": 1550.0031,
+    "yd2": 1.19599,
+    "mile2": 0.000000386102,
 }
+
 
 def square_meters_to_square_feet(base: str, to: str, square_meters: float):
     # Conversion factor from square meters to square feet
@@ -20,12 +21,12 @@ def square_meters_to_square_feet(base: str, to: str, square_meters: float):
     # convert anything to M^2
     m = CONVERSTIONS_TO_M_SQUAR[base]
     square_meters_value = square_meters / m
-    # convert M^2 to target unit    
+    # convert M^2 to target unit
     v = CONVERSTIONS_TO_M_SQUAR[to]
     value = square_meters_value * v
     return value
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    print(square_meters_to_square_feet('km2', 'mile2', 30))
+    print(square_meters_to_square_feet("km2", "mile2", 30))
