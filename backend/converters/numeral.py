e10cf6f5bfa716da14d13b76c4ee1169acce80ed
KingVioletthe1st <kdeemah0@gmail.com>
2025-12-21 10:43:11 +0200

    numeral by abdullah

--- backend/converters/numeral.py
+++ backend/converters/numeral.py
@@ -1,1 +1,22 @@
-# Placeholder module for the Conversion Calculator project.
+try:
+    from . import numbersystem as nt
+except Exception:
+    import numbersystem as nt
+
+conver=["bin","hexa","octa"]
+
+
+def numiral_system(value:int, op:str):
+
+    if op.lower()==conver[0].lower():
+        return nt.decimalToBinary(value)
+
+    elif op.lower()==conver[1].lower():
+        return nt.decimalToHexa(value)
+
+    elif op.lower()==conver[2].lower():
+        return nt.decimalToOctal(value)
+
+if __name__=="__main__":
+    x=numiral_system(12,"bin")
+    print(x)
